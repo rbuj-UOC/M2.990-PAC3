7aef95b1aa801d46e64bac8a41b2eb6c0e4f22ca
Robert Buj <rbuj@users.noreply.github.com>
2025-12-08 08:28:56 +0100

    Initial commit

---
+++ install.R
@@ -0,0 +1,17 @@
+# Install required packages for R-cast course
+install.packages(c(
+  "languageserver", # for R language server support for VSCode
+  "readr", # for reading CSV files
+  "dplyr", # for data manipulation
+  "rmarkdown", # for dynamic report generation
+  "knitr", # for dynamic report generation
+  "ggplot2", # for data visualization
+  "gridExtra", # for arranging multiple ggplots
+  "corrplot", # for correlation plots
+  "caret", # for confusion matrix and other ML utilities
+  "scatterplot3d", # for 3D scatter plots
+  "car", # for regression diagnostics
+  "kableExtra", # for enhanced table formatting
+  "formattable", # for advanced table formatting
+  "tidyr" # for data tidying
+))
